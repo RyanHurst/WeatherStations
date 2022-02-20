00efcca67cf946b67ee72512ca92e616c741ec0c
Ryan Hurst <ryanhurst1@hotmail.com>
2022-02-19 22:29:02 -0700

    use launchIn

--- app/src/main/java/ryanhurst/weather/application/WeatherViewModel.kt
+++ app/src/main/java/ryanhurst/weather/application/WeatherViewModel.kt
@@ -4,8 +4,8 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import ryanhurst.weather.domain.LoadConditionsUseCase
 import ryanhurst.weather.domain.WeatherViewState
 import javax.inject.Inject
@@ -15,10 +15,8 @@ class WeatherViewModel @Inject constructor(private val loadConditionsUseCase: Lo
     ViewModel() {
     val weatherViewState = MutableStateFlow<WeatherViewState>(WeatherViewState.Success(emptyList()))
     fun load(stations: List<String>) {
-        viewModelScope.launch {
-            loadConditionsUseCase.loadConditions(stations).collectLatest {
-                weatherViewState.emit(it)
-            }
-        }
+        loadConditionsUseCase.loadConditions(stations).onEach {
+            weatherViewState.emit(it)
+        }.launchIn(viewModelScope)
     }
 }
