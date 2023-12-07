98c35ced4d38ac74218ac3a856952d7114c1d11b
Ryan Hurst <ryanhurst1@hotmail.com>
2023-12-06 19:40:04 -0700

    bump version. added a privacy policy. new key

--- app/src/main/java/ryanhurst/weather/data/KeyFile.kt
+++ app/src/main/java/ryanhurst/weather/data/KeyFile.kt
@@ -4,5 +4,4 @@ package ryanhurst.weather.data
  * File to hold TOKEN
  * Created by ryan on 1/8/17.
  */
-// todo Don't use this key plz, it's mine. it will stop working if too many people use it. get your own key here https://developers.synopticdata.com/mesonet/
-const val TOKEN = "0aee752c260e4ab4afb385b45b028f6e"
+const val TOKEN = "4129ebf20d3c4022b476739b5524c9b0"
